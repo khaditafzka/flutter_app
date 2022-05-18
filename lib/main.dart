34dc9a348825ff63e7dc3919743cbca99f1f2922
Moustapha KA <khadija231@outlook.com>
2022-05-18 22:40:19 +0000

    lesson17_quotes_use_map_as_js

--- lib/main.dart
+++ lib/main.dart
@@ -1,128 +1,42 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home:Home(),
+  home:QuoteList(),
 ));
 
 //statelessWidget allow us to rerender the app whenever changes are saved
 //StateFulWidget to allow us change state of var on the app
-class Home extends StatefulWidget {
-  const Home({Key? key}) : super(key: key);
+class QuoteList extends StatefulWidget {
+  const QuoteList({Key? key}) : super(key: key);
 
   @override
-  State<Home> createState() => _HomeState();
+  State<QuoteList> createState() => _QuoteListState();
 }
 
-class _HomeState extends State<Home> {
+class _QuoteListState extends State<QuoteList> {
 
-  int ninjaLevel = 0;
+  List<String> quotes =[
+    'Be yourself; everyone else is already taken',
+    'I have nothing to declare except my genius',
+    'The truth is rarely pure and never simple'
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      //color of the app screen
-      backgroundColor: Colors.grey[900],
+      backgroundColor: Colors.grey[200],
       appBar: AppBar(
-        title: Text(
-          'my first app',
-          style: TextStyle(
-            fontSize: 20.0,
-            fontWeight: FontWeight.bold,
-            letterSpacing: 2.0,
-          ),
-        ),
+        title: Text('Awesome Quotes'),
         centerTitle: true,
-        backgroundColor: Colors.grey[850],
-        elevation: 0.0,
+        backgroundColor: Colors.redAccent,
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: (){
-          setState((){
-            ninjaLevel += 1;
-          });
-        },
-        child: Icon(Icons.add),
-        backgroundColor: Colors.grey[800],
+      body: Column(
+        children: quotes.map((quote) => Text(quote)).toList(),
       ),
-      body: Padding(
-        padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            //Avatar
-            Center(
-              child: CircleAvatar(
-                backgroundImage: AssetImage('assets/robert-wiedemann-xwdWtPpKDeU-unsplash.jpg'),
-                radius: 40.0,
-              ),
-            ),
-            Divider(
-              height: 80.0,
-              color: Colors.grey[800],
-            ),
-            Text(
-                'NAME',
-              style: TextStyle(
-                color: Colors.grey,
-                letterSpacing: 2.0,
-              ),
-            ),
-            //add space beetween widgets
-            SizedBox(height: 10.0,),
-            Text(
-              '@Khaditafzka',
-              style: TextStyle(
-                color: Colors.deepOrange,
-                letterSpacing: 2.0,
-                fontWeight: FontWeight.bold,
-                fontSize: 28.0
-              ),
-            ),
-            SizedBox(height: 30.0,),
-            Text(
-              'current Ninja Level',
-              style: TextStyle(
-                color: Colors.grey,
-                letterSpacing: 2.0,
-              ),
-            ),
-            //add space beetween widgets
-            SizedBox(height: 10.0,),
-            Text(
-              '$ninjaLevel',
-              style: TextStyle(
-                  color: Colors.deepOrange,
-                  letterSpacing: 2.0,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 28.0
-              ),
-            ),
-            SizedBox(height: 30.0,),
-            Row(
-              children: [
-                Icon(
-                  Icons.mail,
-                  color: Colors.blueGrey[200],
-                ),
-                SizedBox(width: 10.0,),
-                Text(
-                  'khadija231@outlook.com',
-                  style: TextStyle(
-                    color: Colors.blueGrey[200],
-                    fontSize: 18.0,
-                    letterSpacing: 1.0
-                  ),
-                )
-              ],
-            ),
-
-          ],
-        ),
-      ),
-
     );
   }
 }
+
 
 
 
